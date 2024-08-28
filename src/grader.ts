e19e12d856dacf7af62bd9ed92de5eda409965ea
Dimas Saputra <dimas.maulanads@gmail.com>
2024-08-28 16:05:36 +0700

    complete the `contain-main-js` acceptance scenario

--- src/grader.ts
+++ src/grader.ts
@@ -1,6 +1,7 @@
 import { checkContainPackageJson } from './criterias/contain-package-json';
-import { getSubmissionInfo } from './utils';
+import { findFolderBaseOnFile, getSubmissionInfo } from './utils';
 import { buildReport } from './report';
+import { checkContainMainJs } from './criterias/contain-main-js';
 
 export async function grade(submissionPath: string) {
   const submissionInfo = await getSubmissionInfo(submissionPath);
@@ -8,5 +9,20 @@ export async function grade(submissionPath: string) {
   // check contain package.json criteria
   const containPackageJsonChecklist = await checkContainPackageJson(submissionPath);
 
-  return buildReport([containPackageJsonChecklist], submissionInfo, submissionPath);
+  // if the submission not contain package.json, we can't any further do
+  if (!containPackageJsonChecklist.completed) {
+    return buildReport([containPackageJsonChecklist], submissionInfo, submissionPath);
+  }
+
+  // we assume folder that contain package.json is the project path
+  const projectPath = await findFolderBaseOnFile(submissionPath, 'package.json');
+
+  // check contain main.js checklist
+  const containMainJSChecklist = await checkContainMainJs(projectPath);
+
+  return buildReport(
+    [containPackageJsonChecklist, containMainJSChecklist],
+    submissionInfo,
+    submissionPath
+  );
 }
