7cecf0f82710fe9fd2b91b6c5265eb6fc53c9cab
Dimas Saputra <dimas.maulanads@gmail.com>
2024-08-29 16:24:42 +0700

    floor logger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { grade } from './grader';
+import { logger } from './logging';
 
 const argv = yargs(hideBin(process.argv))
   .option('path', {
@@ -13,4 +14,5 @@ const argv = yargs(hideBin(process.argv))
   const { path } = await argv;
 
   await grade(path as string);
+  logger.info(`submission has successfully graded in ${process.uptime().toFixed(2)} seconds`);
 })();
